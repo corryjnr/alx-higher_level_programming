a87662eadadfd05c82b715791f273588ab9a464b
Michael Otieno <michaelcorry01@gmail.com>
2023-01-11 07:34:42 +0000

    Max value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if my_list:
+        maxi = 0
+        for i in my_list:
+            if i > maxi:
+                maxi = i
+        return maxi
+    else:
         return None
-    maxi = 0
-    for i in my_list:
-        if i > maxi:
-            maxi = i
-    return maxi
