43364f9ad0f008976f81bb7fd99b9ec52c0b93d9
Michael Otieno <michaelcorry01@gmail.com>
2023-01-11 07:22:30 +0000

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
+    if sentence:
+        first = sentence[0]
+    else:
+        first = None
     return length, first
