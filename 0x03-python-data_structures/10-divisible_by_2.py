7a481c255bf224172af47ac058e6b876efa6ad95
Michael Otieno <michaelcorry01@gmail.com>
2023-01-11 09:39:37 +0000

    divisibility test

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    test_res = []
+    for i in range(0, len(my_list)):
+        if i % 2 == 0:
+            test_res.append(True)
+        else:
+            test_res.append(False)
+    return test_res
